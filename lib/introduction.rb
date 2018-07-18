a63e0ec828e724931d1a6f51ee67906a8ea085e7
McKenzie Fiege <mckenziefiege@gmail.com>
2018-07-18 01:36:01 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,11 @@
 #lib/introduction.rb
 
-def introduction_with_language(name, language)
-  puts "Hi my name is #{name} and I am learning to program in #{language}."
-end
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end 
 
-introduction_with_language("Kenzie", "Ruby")
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
